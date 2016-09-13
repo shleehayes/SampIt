bf02de786710a4650637dfc8f577a4e696865b73
Ashlee Christine Hayes <ACH@Ashlees-MacBook-Pro.local>
2016-09-13 13:12:59 -0700

    added plugin, cleaned up code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,29 +1,20 @@
 $( document ).ready(function() {
-	
-  $("#camera_button").bind('touchstart', function(){
-		navigator.camera.getPicture(onSuccess, onFail, 
-			{ 
-        quality: 75,
-    		destinationType: Camera.DestinationType.DATA_URL,
-    		sourceType: Camera.PictureSourceType.CAMERA,
-        allowEdit: true,
-			  encodingType: Camera.EncodingType.JPEG,
-			  cameraDirection: 1,
-        targetWidth: 600,
-        targetHeight: 1100,
-        popoverOptions: CameraPopoverOptions,
-        saveToPhotoAlbum: true
-      })
-  });
-
-  function onSuccess(imageData) {
-      var $image = $("#image_store");
-      $image.attr("src", "data:image/jpeg;base64," + imageData);
-  }
-
-  function onFail(message) {
-      alert('Failed because: ' + message);
-  }
-
+	  $('input.color').colorPicker();
+    // description of option will follow
+    $('input.color').colorPicker({
+        klass: window.ColorPicker,
+        input: elm,
+        patch: elm,
+        init: function(elm, colors){}, // initialization callback (before colorPicker gets initialized though)
+        animationSpeed: 200,
+        draggable: true,
+        multipleInstances: false
+        margin: {left: -1, top: 2},
+        initStyle: 'display: none',
+        mode: 'hsv-h',
+        size: 1,
+        renderCallback: renderCallback
+        // and all other options from color and colorPicker
+    });
 });
 
